220e3146996e8f1a393f6da33c81b6e8619e819c
Abewas1493 <95421359+Abewas1493@users.noreply.github.com>
2024-03-13 15:19:51 +0300

    Delete Water Quality Prediction directory

--- Water Quality Prediction/App/static/JavaScript/HomeScript.js
+++
@@ -1,7 +0,0 @@
-            document.querySelector('.logo').addEventListener('mouseenter', function () {
-                document.body.classList.add('overlay');
-            });
-
-            document.querySelector('.logo').addEventListener('mouseleave', function () {
-                document.body.classList.remove('overlay');
-            });
