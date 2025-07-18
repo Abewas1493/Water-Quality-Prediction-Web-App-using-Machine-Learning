5b07a394d7b8f9b10a4f35b0e68fbf043cb44e6b
Abewas1493 <95421359+Abewas1493@users.noreply.github.com>
2025-07-18 19:26:40 +0300

    Delete Water Quality Prediction directory

--- Water Quality Prediction/App/Info.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from . import app
-
-@app.route("/info", methods=["POST", "GET"])
-def info():
-    return render_template("info.html")
--- Water Quality Prediction/App/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.secret_key ='tfdvcgsdbvdbvbcvbsvbdsvsv'
-from .import Home, Dashboard, Predict, Info, Download   
